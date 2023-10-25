bc5bb469815473e96f5e5dac05b03737b0d957d5
Nick Knysh <46058617+QQumba@users.noreply.github.com>
2023-10-25 12:27:03 +0300

    add unit tests

---
+++ api/FakeUnitTestProj/FakeTests.cs
@@ -0,0 +1,15 @@
+﻿namespace FakeUnitTestProj;
+
+public class FakeTests
+{
+    public void TestAddition()
+    {
+        var actual = 2 + 3;
+        var expected = 5;
+
+        if (actual != expected)
+        {
+            throw new Exception();
+        }
+    }
+}
